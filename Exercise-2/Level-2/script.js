b94849f97f61188016c3b12b4fe3897a4a316df0
shrivastavpiyush23 <piyush.srivastava_cs19@gla.ac.in>
2022-04-26 01:48:21 +0530

    first commit

---
+++ Exercise-2/Level-2/script.js
@@ -0,0 +1,37 @@
+// 1.
+// var base = window.prompt("Enter base :");
+// var height = window.prompt("Enter height :");
+// var area = 0.5 * base * height;
+// document.write(area);
+
+// 2.
+// var side1 = window.prompt("Enter side 1 :"); 
+// var side2 = window.prompt("Enter side 2 :");
+// var side3 = window.prompt("Enter side 3 :");
+// var perimeter = (parseInt(side1) + parseInt(side2) + parseInt(side3));
+// console.log(perimeter);
+
+// 3.
+// var side1 = window.prompt("Enter side1");
+// var side2 = window.prompt("Enter side2");
+// var area = (side1 * side2);
+// var perimeter = (parseInt(side1) + parseInt(side2));
+// console.log(area);
+// console.log(perimeter);
+
+// 4.
+// var radius = window.prompt("Enter radius :");
+// const pi = 3.14;
+// var area = (pi * radius * radius);
+// var circumference = (2 * pi * radius);
+// console.log(area);
+// console.log(circumference);
+
+// 6.
+// var x1 = 2;
+// var x2 = 6;
+// var y1 = 2;
+// var y2 = 10;
+
+// var m = (y2-y1)/(x2-x1);
+// console.log(m);
