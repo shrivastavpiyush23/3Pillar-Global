8d23ce023f7e3e3dfe440a0e9138b40072febac7
shrivastavpiyush23 <piyush.srivastava_cs19@gla.ac.in>
2022-05-09 12:55:40 +0530

    level-2 completed

--- Exercise-3/Level-2/script.js
+++ Exercise-3/Level-2/script.js
@@ -34,3 +34,39 @@ console.log(shoppingCart3);
 const shoppingCart4 = ['Milk', 'Coffee', 'Tea', 'Honey'];
 shoppingCart4[2] = "Green Tea";
 console.log(shoppingCart4);
+
+// 4.
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+if(countries.includes('Ethiopia')) console.log('ETHIOPIA');
+else countries.push('Ethiopia'); console.log(countries);
+
+// 5.
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ];
+  if(webTechs.includes('Sass')) console.log('Sass is CSS preprocess.');
+  else webTechs.push('Sass'); console.log(webTechs);
+
+// 6.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
