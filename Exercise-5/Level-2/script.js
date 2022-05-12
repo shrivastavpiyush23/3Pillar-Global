1b8c482fd72d24bcd8da4cf9f8f2f174ae9484ff
shrivastavpiyush23 <piyush.srivastava_cs19@gla.ac.in>
2022-05-12 09:24:02 +0530

    level-2

---
+++ Exercise-5/Level-2/script.js
@@ -0,0 +1,8 @@
+// 1.
+let marks = window.prompt("Enter your marks : ");
+if(marks > 90 && marks <= 100) document.write('A');
+else if(marks > 70 && marks <= 90) document.write('B');
+else if(marks > 60 && marks <= 70) document.write('C');
+else if(marks > 50 && marks <= 60) document.write('D');
+else if(marks > 0 && marks <= 60) document.write('F');
+
